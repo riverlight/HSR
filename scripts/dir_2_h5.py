03798d34b3f8763dc5b0502e375a7383a25311a5
riverlight <heliang1108@vip.sina.com>
2021-06-08 14:08:22 +0800

    更新了 dir_2_h5.py，支持新的 h5中rgb 的数据格式

--- scripts/dir_2_h5.py
+++ scripts/dir_2_h5.py
@@ -10,9 +10,10 @@ import numpy as np
 patch_size = 96
 stride = patch_size * 1
 
-def main(dir, h5_name):
+def main(dir, h5_name, mode):
     print("dir : ", dir)
     print("h5_name : ", h5_name)
+    print('mode : ', mode.upper())
     h5_file = h5py.File(h5_name, 'w')
     hr_patchs = list()
 
@@ -23,13 +24,18 @@ def main(dir, h5_name):
         print("id : ", count, imagename)
         # BGR HWC
         hr_img = cv2.imread(imagename, cv2.IMREAD_UNCHANGED)
-        # BGR HWC to RGB CHW
-        hr_img = hr_img[:, :, [2, 1, 0]].transpose(2, 0, 1)
-
-        for i in range(0, hr_img.shape[1] - patch_size + 1, stride):
-            for j in range(0, hr_img.shape[2] - patch_size + 1, stride):
-                hr_np = hr_img[:, i:i + patch_size, j:j + patch_size]
-                hr_patchs.append(hr_np)
+        if mode=='CHWRGB':
+            # BGR HWC to RGB CHW
+            hr_img = hr_img[:, :, [2, 1, 0]].transpose(2, 0, 1)
+            for i in range(0, hr_img.shape[1] - patch_size + 1, stride):
+                for j in range(0, hr_img.shape[2] - patch_size + 1, stride):
+                    hr_np = hr_img[:, i:i + patch_size, j:j + patch_size]
+                    hr_patchs.append(hr_np)
+        else: # HWCBGR
+            for i in range(0, hr_img.shape[0] - patch_size + 1, stride):
+                for j in range(0, hr_img.shape[1] - patch_size + 1, stride):
+                    hr_np = hr_img[i:i + patch_size, j:j + patch_size, :]
+                    hr_patchs.append(hr_np)
 
     hr_ds = np.array(hr_patchs, dtype=np.uint8)
     h5_file.create_dataset('hr', data=hr_ds)
@@ -38,7 +44,12 @@ def main(dir, h5_name):
     pass
 
 if __name__=="__main__":
-    if len(sys.argv)!=3:
-        print("python3 dir_2_h5.py dir_name h5_name")
+    if len(sys.argv)!=4:
+        print("python3 dir_2_h5.py dir_name h5_name mode")
+        print('mode must be HWCBGR or CHWRGB')
+        print("sample : python3 dir_2_h5.py D:/workroom/tools/image/ntire20/Corrupted-tr-y ../qn_dataset/vsr_train_hwcbgr.h5 HWCBGR")
         exit(-1)
-    main(sys.argv[1], sys.argv[2])
+    if sys.argv[3].lower() not in ['hwcbgr', 'chwrgb']:
+        print('mode must be HWCBGR or CHWRGB')
+        exit(-2)
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
